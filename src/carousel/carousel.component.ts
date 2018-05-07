539b2abacdcba189c67a5e18c952adc9a1620f0b
Stephen Legge <stephen@aleggeup.com>
2018-05-07 08:44:43 -0400

    Change selector to name of component

--- src/carousel/carousel.component.ts
+++ src/carousel/carousel.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-carousel',
+  selector: 'ngx-parallax-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
