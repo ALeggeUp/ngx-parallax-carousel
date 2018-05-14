25981df7609f3be9ffd2fc9010c57666c0bce757
Stephen Legge <stephen@aleggeup.com>
2018-05-14 02:25:50 -0400

    Set up some of the bindings and listeners that will be needed

--- src/ngx-parallax-carousel/ngx-parallax-carousel.component.ts
+++ src/ngx-parallax-carousel/ngx-parallax-carousel.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'ngx-parallax-carousel',
+  exportAs: 'ngxParallaxCarousel',
   templateUrl: './ngx-parallax-carousel.component.html',
   styleUrls: ['./ngx-parallax-carousel.component.css']
 })
 export class NgxParallaxCarouselComponent implements OnInit {
+
+  @HostBinding('class.isActive')
+  public isActive: boolean = true;
+
+  @HostBinding('style.display')
+  public styleDisplay: string = 'block';
 
   constructor() {
     console.debug('NgxParallaxCarouselComponent constructor');
@@ -13,5 +20,34 @@ export class NgxParallaxCarouselComponent implements OnInit {
 
   public ngOnInit() {
     console.debug('NgxParallaxCarouselComponent init');
+  }
+
+  @HostListener('mouseenter')
+  public pause() {
+    console.log('pause');
+  }
+
+  @HostListener('mouseleave')
+  public cycle() {
+    console.log('cycle');
+  }
+
+  public prev() {
+    console.log('prev');
+  }
+
+  public next() {
+    console.log('next');
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  public handleKeyboardEvent(event: KeyboardEvent): void {
+    console.log(event.key);
+    event.stopPropagation();
+    if (event.key === 'ArrowLeft') {
+      this.prev();
+    } else if (event.key === 'ArrowRight') {
+      this.next();
+    }
   }
 }
