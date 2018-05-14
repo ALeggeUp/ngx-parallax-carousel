f079cab30a32492ef0fa108d8d974e3ad6ea752a
Stephen Legge <stephen@aleggeup.com>
2018-05-14 01:32:25 -0400

    Fix up linter warnings

--- src/carousel/carousel.component.ts
+++ src/carousel/carousel.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.debug('CarouselComponent constructor');
   }
 
+  public ngOnInit() {
+    console.debug('CarouselComponent init');
+  }
 }
